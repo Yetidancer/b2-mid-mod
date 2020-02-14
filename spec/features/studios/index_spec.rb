a45e6a5a1217dfcfd541ac952022cae4f55f1804
Ezekiel Clark <clarkzeke@gmail.com>
2020-02-14 09:20:26 -0700

    Add studio and movie tables

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -8,6 +8,28 @@ RSpec.describe 'studio index page' type: :feature do
       @movie_1 = Movie.create(name: "Parasite",
                               creation: "2019",
                               genre: "Horror")
+      @movie_2 = Movie.create(name: "Titanic",
+                              creation: "2000",
+                              genre: "Drama")
+      @movie_3 = Movie.create(name: "Get Out",
+                              creation: "2016",
+                              genre: "Horror")
+      @movie_4 = Movie.create(name: "Toy Story",
+                              creation: "1995",
+                              genre: "Childrens")
+      @studio_1 = Studio.create(name: "A24")
+      @studio_1 = Studio.create(name: "Warner Brothers")
+    end
+
+    it 'should display movie names on studio index page' do
+      visit '/studios'
+
+      expect(page).to have_content("A24")
+      expect(page).to have_content("Parasite")
+      expect(page).to have_content("Titanic")
+      expect(page).to have_content("Warner Brothers")
+      expect(page).to have_content("Get Out")
+      expect(page).to have_content("Toy Story")
     end
 
   end
